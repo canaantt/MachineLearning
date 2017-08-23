b5d604414a778c81a5edb977f77868d09b87d534
canaantt <canaantt@gmail.com>
2017-08-23 14:12:44 -0700

    split training and test sets

--- partI_data_processing/data_preprocessing.py
+++ partI_data_processing/data_preprocessing.py
@@ -5,7 +5,7 @@ import pandas as pd
 # importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
@@ -17,4 +17,11 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit(X[:, 0])
-oneHotEncoder = OneHotEncoder()
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
--- partI_data_processing/data_proecessing.R
+++ partI_data_processing/data_proecessing.R
@@ -5,3 +5,16 @@ dataset$Age = ifelse(is.na(dataset$Age),
 dataset$Salary = ifelse(is.na(dataset$Salary),
               ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
               dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         level = c('France', 'Spain', 'Germany'),
+                         label = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchase,
+                           level = c('No','Yes'),
+                           label = c(0, 1))
+library(caTools)
+set.seed(123)
+split =  sample.split(dataset$Purchase, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
